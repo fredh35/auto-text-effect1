8348d626d826e1f70e89536cce9afb8563f7ca72
fredh35 <35115158+fredh35@users.noreply.github.com>
2021-08-18 09:12:31 -0400

    Add files via upload

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'Aime ta belle épouse!'
 let idx = 1
 let speed = 300 / speedEl.value
 
